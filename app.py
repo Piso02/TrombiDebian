c475fe5db70c10e1baf245caf5442d78dc5f83c2
rrasoamahefa <rrasoamahefa@fthmconsulting.com>
2021-08-16 12:48:34 +0000

    templateOk

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 import sqlite3
 
-from flask import Flask, render_template, request, redirect, url_for, flash
-from views.global_ldap_authentication import *
-from forms.LoginForm import *
-from flask.sessions import SecureCookieSessionInterface
-from itsdangerous import URLSafeTimedSerializer
+from flask import Flask, render_template
+
+
+
 
 
 def get_db_connection():
@@ -16,29 +15,11 @@ def get_db_connection():
 app = Flask(__name__)
 
 
-@app.route('/login', methods=['GET','POST'])
-def index():
+@app.route('/')
+def login():
     
-      # initiate the form..
-    form = LoginValidation()
- 
-    if request.method in ('POST') :
-        login_id = form.user_name_pid.data
-        login_password = form.user_pid_Password.data
- 
-        # create a directory to hold the Logs
-        login_msg = global_ldap_authentication(login_id, login_password)
- 
-        # validate the connection
-        if login_msg == "Success":
-            success_message = f"*** Authentication Success "
-            return render_template('success.html', success_message=success_message)
- 
-        else:
-            error_message = f"*** Authentication Failed - {login_msg}"
-            return render_template("error.html", error_message=str(error_message))
- 
-    return render_template('login.html', form=form)
+      
+    return render_template('login.html')
  
 
 
